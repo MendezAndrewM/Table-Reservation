441bb402ee93396edab6b6c4653f3fdb00ae47b4
unknown <Mendez.AndrewM@gmail.com>
2019-08-08 19:28:58 -0700

    added empty url paths

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 const PORT = 3000;
@@ -6,6 +7,16 @@ const PORT = 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+
+app.get("/"), (req, res) => {
+    res.sendFile(path.join(__dirname, ""));
+};
+app.get("/"), (req, res) => {
+    res.sendFile(path.join(__dirname, ""));
+};
+app.get("/"), (req, res) => {
+    res.sendFile(path.join(__dirname, ""));
+};
 
 
 app.listen(PORT, function() {
